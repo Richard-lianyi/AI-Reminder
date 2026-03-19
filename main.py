3bcd643c0a25b68ff90ef687bbe290cbd665ae8e
Richard-lianyi <lchen275@uottawa.ca>
2026-03-19 01:20:29 -0400

    change UI and update main file logic

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 from database import init_db
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 init_db()
 
